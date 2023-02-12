1baf7f006a029aa0b2c04e89d25797ab483d08dc
Joseph Onyach <josephokothonyach@gmail.com>
2023-02-12 17:03:02 +0300

    Variables in rust

---
+++ hello/hello/target/debug/deps/hello-db4c0c1082408879.d
@@ -0,0 +1,5 @@
+/home/joseph/rust-lang/hello/hello/target/debug/deps/hello-db4c0c1082408879: src/main.rs
+
+/home/joseph/rust-lang/hello/hello/target/debug/deps/hello-db4c0c1082408879.d: src/main.rs
+
+src/main.rs:
---
+++ hello/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/joseph/rust-lang/hello/hello/target/debug/hello: /home/joseph/rust-lang/hello/hello/src/main.rs
