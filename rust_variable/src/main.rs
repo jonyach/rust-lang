a388b7f6f84ecde0d32c6d7aa35bd688f859b634
Joseph Onyach <josephokothonyach@gmail.com>
2023-02-12 17:06:06 +0300

    Variables in rust language

--- rust_variable/src/main.rs
+++ rust_variable/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    
+    let year_of_study = "Year Two";
+    let semester = 1;
+    let course = "Microprocessor Technology and Instrumentation";
+    let number_of_units = 7;
+    let encouragement = "Your're good to go, kindly study and pass 6 units to be promoted to the next level of study";
+
+    println! ("Your year of study is {}", year_of_study);
+    println! ("You're in Semester {}", semester);
+    println! ("Your course is {}", course);
+    println! ("The number of units you're currently registered for are: {}", number_of_units);
+    println! ("Hurray! {}", encouragement);
 }
